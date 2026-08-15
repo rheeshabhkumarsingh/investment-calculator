3d67fb8a9b277104336758b3180ab17ce02654b6
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-08-15 23:46:41 +0530

    required component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { UserInput } from './user-input/user-input';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, UserInput],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
