51536984de216abd88a3606dd8758e21df8f3ed3
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-08-20 02:20:03 +0530

    ng form added

--- src/app/user-input/user-input.ts
+++ src/app/user-input/user-input.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.html',
   styleUrl: './user-input.css',
 })
 export class UserInput {
 
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '0';
+  duration = '0';
+
+  onSubmit() {
+     
+  }
 }
