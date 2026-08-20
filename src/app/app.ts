d47ae73394384a40ecf5305bb89f3f5cfa933bb7
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-08-21 01:10:24 +0530

    investment calulator logic copied

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,5 +10,30 @@ import { UserInput } from './user-input/user-input';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('investment-calculator');
+  calculateInvestmentResult(data: {
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number
+  }) {
+    const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for(let i = 0; i<duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment*year,
+      })
+    }
+
+    return annualData;
+  }
 }
